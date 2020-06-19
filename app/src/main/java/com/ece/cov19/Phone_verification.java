e6d98d8fb83e3bbdcde698368b7df11deb4a3ee7
Khalid Syfullah <Khalidsyfullah@gmail.com>
2020-06-19 15:40:24 +0600

    Test
    Test

--- app/src/main/java/com/ece/cov19/Phone_verification.java
+++ app/src/main/java/com/ece/cov19/Phone_verification.java
@@ -23,7 +23,7 @@ public class Phone_verification extends AppCompatActivity {
         setContentView(R.layout.activity_phone_verification);
 
         backBtn=findViewById(R.id.phn_ver_back_button);
-
+//hello
         labelPhone=findViewById(R.id.phn_ver_phone_text);
         countryCode=findViewById(R.id.phn_ver_countryCode);
         getOtpBtn=findViewById(R.id.phn_ver_get_otp_button);
