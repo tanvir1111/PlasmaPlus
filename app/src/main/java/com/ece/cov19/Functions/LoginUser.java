bdccfe38192760d969191b77f3d541f9263abcc1
tanvir1111 <53863352+tanvir1111@users.noreply.github.com>
2020-07-26 16:42:45 +0600

    login user created and used in loginactivity
    need a reclone

---
+++ app/src/main/java/com/ece/cov19/Functions/LoginUser.java
@@ -0,0 +1,103 @@
+package com.ece.cov19.Functions;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+
+import com.ece.cov19.DashboardActivity;
+import com.ece.cov19.DataModels.UserDataModel;
+import com.ece.cov19.LoginActivity;
+import com.ece.cov19.R;
+import com.ece.cov19.RetroServices.RetroInstance;
+import com.ece.cov19.RetroServices.RetroInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.Context.MODE_PRIVATE;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserAge;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserBloodGroup;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserDistrict;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserDivision;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserDonorInfo;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserGender;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserName;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserPass;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserPhone;
+import static com.ece.cov19.DataModels.LoggedInUserData.loggedInUserThana;
+import static com.ece.cov19.LoginActivity.LOGIN_SHARED_PREFS;
+import static com.ece.cov19.LoginActivity.LOGIN_USER_PASS;
+import static com.ece.cov19.LoginActivity.LOGIN_USER_PHONE;
+
+public class LoginUser {
+    private static String loginstat;
+    public static void loginUser(Context ctx, String phone, String password,Class<?> toActivity,String from ){
+
+
+        RetroInterface retroInterface = RetroInstance.getRetro();
+        Call<UserDataModel> sendingData = retroInterface.loginRetroMethod(phone, password);
+        sendingData.enqueue(new Callback<UserDataModel>() {
+            @Override
+            public void onResponse(Call<UserDataModel> call, Response<UserDataModel> response) {
+                if (response.body().getServerMsg().toLowerCase().equals("success")) {
+
+                    ToastCreator.toastCreatorGreen(ctx, ctx.getResources().getString(R.string.welcome)+" " + response.body().getName());
+
+//                  Storing phone and password to shared preferences
+                    SharedPreferences loginSharedPrefs = ctx.getSharedPreferences(LOGIN_SHARED_PREFS, MODE_PRIVATE);
+                    SharedPreferences.Editor loginPrefsEditor = loginSharedPrefs.edit();
+                    loginPrefsEditor.putString(LOGIN_USER_PHONE, response.body().getPhone());
+                    loginPrefsEditor.putString(LOGIN_USER_PASS, response.body().getPassword());
+                    loginPrefsEditor.apply();
+
+//              setting all logged in info
+                    loggedInUserName = response.body().getName();
+                    loggedInUserPhone = response.body().getPhone();
+                    loggedInUserGender = response.body().getGender();
+                    loggedInUserBloodGroup = response.body().getBloodGroup();
+                    loggedInUserDivision = response.body().getDivision();
+                    loggedInUserDistrict = response.body().getDistrict();
+                    loggedInUserThana = response.body().getThana();
+                    loggedInUserAge = response.body().getAge();
+                    loggedInUserDonorInfo = response.body().getDonor();
+                    loggedInUserPass = response.body().getPassword();
+                    loginstat = "successful";
+
+
+//                  going to Dashboard
+                    Intent intent = new Intent(ctx, toActivity);
+                    intent.putExtra("from",from);
+
+//                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    ctx.startActivity(intent);
+
+                }
+                else if(response.body().getServerMsg().toLowerCase().equals("wrong phone or password")){
+
+                    ToastCreator.toastCreatorRed(ctx,ctx.getResources().getString(R.string.login_activity_wrong));
+                }
+
+                else {
+
+
+                    ToastCreator.toastCreatorRed(ctx,ctx.getResources().getString(R.string.connection_error));
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<UserDataModel> call, Throwable t) {
+
+
+
+                ToastCreator.toastCreatorRed(ctx,ctx.getResources().getString(R.string.connection_error));
+            }
+        });
+
+
+    }
+
+}
