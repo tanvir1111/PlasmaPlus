295e4202d95b519446b7e2b5a9778f107fffadcf
Khalid Syfullah <Khalidsyfullah@gmail.com>
2021-11-27 00:59:32 +0600

    nodeJS Update 8
    Patient Deadline Notification Update
    Patient Auto-Deletion Update

--- node.js/server.js
+++ node.js/server.js
@@ -1,10 +1,13 @@
-const mysql = require("mysql");
-const express = require("express");
-const app = express();
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+const mysql = require("mysql")
+const express = require("express")
+const app = express()
+var https = require('https')
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+var cron = require("node-cron")
+var async = require("async")
+var admin = require("./app/models/firebase.js")
 
 var options = {
   key: fs.readFileSync(path.resolve('sslcert/selfsigned.key')),
@@ -65,3 +68,193 @@ app.get("/", (req,res) => {
 });
 
 
+
+setInterval(headerPrint, 10000);
+cron.schedule('00 00 * * *', patientDeadlineNotification)
+cron.schedule('15 00 * * *', patientAutoDeletion)
+
+
+function headerPrint(){
+
+    var date = new Date()
+    console.log("Auto-script: "+date)
+}
+
+function patientDeadlineNotification(){
+
+    
+
+    var date = new Date()
+    date.setDate(date.getDate() + 0)
+    var dateString = ("0" + date.getDate()).slice(-2)+"-"+("0" + (date.getMonth() + 1)).slice(-2)+"-"+date.getFullYear()
+    
+    console.log("Patient Deadline Notification: "+dateString)
+
+
+    var sql = "SELECT * FROM patients WHERE date = ?"
+    db.query(sql, dateString, (err, data) => {
+
+        if(err) throw err
+        if(data.length > 0){
+
+            console.log("Patients found: "+data.length)
+
+            async.forEachOf(data, (row, i, callback) => {
+                
+                sendNotification(row.phone, "Patient '"+row.name+"' will expire in 48 hours.","Patient donation deadline is at 12:15 AM on "+row.date+". You can change the deadline from My Patients.","MyPatientsActivity","")
+
+            },
+            function(err){
+                if(err) throw err
+            })
+
+        }
+        else{
+            console.log("Patients found: "+data.length)
+
+        }
+    })
+            
+}
+
+function patientAutoDeletion(){
+
+
+    var date = new Date()
+    date.setDate(date.getDate() + 2)
+    var dateString = ("0" + date.getDate()).slice(-2)+"-"+("0" + (date.getMonth() + 1)).slice(-2)+"-"+date.getFullYear()
+
+    console.log("Patient Auto-Deletion: "+dateString)
+
+    var sql = "SELECT * FROM patients WHERE date = ?"
+    db.query(sql, dateString, (err, data) => {
+
+        if(err) throw err
+        if(data.length > 0){
+
+            console.log("Patients found: "+data.length)
+
+            var length = data.length
+
+            async.forEachOf(data, (row, i, callback) => {
+                
+                sendNotification(row.phone, "Patient '"+row.name+"' has expired.","Patient has been deleted from My Patients.","MyPatientsActivity","")
+                
+                if(i == length - 1){
+
+                    var sql2 = "DELETE FROM patients WHERE date = ?"
+                    db.query(sql2, dateString, (err, result) => {
+
+                        if(err) throw err
+
+                        if(result.affectedRows > 0){
+                            
+                            console.log("Patients deleted: "+result.affectedRows)
+                        }
+                    })
+                }
+            },
+            function(err){
+                if(err) throw err
+            })
+
+        }
+        else{
+            console.log("Patients found: "+data.length)
+
+        }
+    })
+}
+
+
+
+function sendNotification(phone, title, body, activity, hidden){
+        
+    var sql = "SELECT * FROM tokens WHERE phone = ?"
+    db.query(sql,[phone], (err, result) => {
+  
+      if(err) throw err
+  
+      if(result.length > 0){
+    
+        async.forEachOf(result, (result, i, callback) => {
+  
+          const message = {
+      
+            notification: {
+              title: title,
+              body: body
+              },
+            data: {
+              activity: activity,
+              hidden: hidden
+            },
+      
+            token: result.token
+      
+          }
+    
+          admin.messaging().send(message)
+          .then((response) => {
+        
+            console.log('Successfully sent message:', response)
+        
+          })
+          .catch((error) => {
+            console.log('Error sending message:', error)
+        
+          })
+  
+  
+          var sql2 = "SELECT * FROM notifications WHERE phone = ?"
+          db.query(sql2, [phone], (err, result2) => {
+  
+            if(err) throw err
+  
+            if(result2.length == 0){
+  
+                var sql3 = "INSERT INTO notifications (phone, activity) VALUES (?)"
+                var values = [phone, activity]
+                db.query(sql3, [values], (err, result3) => {
+                
+                  if(err) throw err
+                  
+                  if(result3.affectedRows > 0){
+  
+                    console.log('Notification inserted')
+  
+                  }
+                })
+            }
+            else{
+  
+                var sql3 = "UPDATE notifications SET activity = ? WHERE phone = ?"
+                db.query(sql3, [phone, activity], (err, result3) => {
+                
+                  if(err) throw err
+  
+                  if(result3.affectedRows > 0){
+                    
+                    console.log('Notification inserted')
+  
+                  }
+                })
+            }
+            
+          
+        }, 
+        function(err){
+          if(err) throw err
+        })
+  
+        
+  
+  
+  
+      })
+    }
+      else{
+        console.log('No token found')
+      }
+    })     
+  }
